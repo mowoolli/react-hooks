665934acf99c164f85ae9a8a47dcaaf5b4adf51d
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-30 12:36:35 -0700

    Add table and some hardcoded dummy data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -12,6 +19,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
